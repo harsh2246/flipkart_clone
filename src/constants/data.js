0052d21280b615ff80bbd3ea3230b96d14bcddfa
harsh mangal <70967865+harsh2246@users.noreply.github.com>
2024-03-19 12:21:10 +0530

    increase no of items to check scrollable

--- src/constants/data.js
+++ src/constants/data.js
@@ -201,6 +201,23 @@ const cartItem=[{
         description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
         discount: 'Minimum 50% Off', 
         tagline: 'Grab Now!' 
+    },{ 
+        id: 'product7',
+        url: 'https://rukminim1.flixcart.com/image/150/150/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70',
+        detailUrl: 'https://rukminim1.flixcart.com/image/416/416/kcgk1ow0/headphone/n/u/a/235v2-fast-charging-boat-original-imaftk6us4af7bca.jpeg?q=70', 
+        title: {
+            shortTitle: 'Headphones',
+            longTitle: 'boAt Rockerz 235v2 with ASAP charging Version 5.0 Bluetooth Headset '
+        }, 
+        price: {
+            mrp: 2990,
+            cost: 1199,
+            discount: '59%'
+        },
+        quantity: 1,
+        description: 'Let music brighten up your mood anytime, anywhere with the boAt 235v2 Fast Charging Bluetooth Headset. This Bluetooth headset features a Call Vibration Alert, a Fast Charging Technology, and Easy Access Controls to listen to and manage your favorite music with ease.',
+        discount: 'Minimum 50% Off', 
+        tagline: 'Grab Now!' 
     }
 ];
 export {cartItem,imageURL,products,bannerData,navData}
